4032ca4a4c01d57dfd8f016f467fd01a82125e4e
Ruslan <overbah98@gmail.com>
2024-02-05 19:42:27 +0700

    1

--- shop-api/src/main/java/org/example/shop/services/PriceServiceApi.java
+++ shop-api/src/main/java/org/example/shop/services/PriceServiceApi.java
@@ -6,6 +6,8 @@ import lombok.experimental.FieldDefaults;
 import org.example.shop.store.entities.GoodEntity;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
@@ -25,9 +27,30 @@ public class PriceServiceApi {
         } catch (InterruptedException e) {
         }
 
+        return remoteCalculatePriceInRubles(good.getId());
+    }
+
+    public Map<Long, Long> getGoodIdToPriceInRublesMap(List<GoodEntity> goods) {
+
+        // Обращение к отдельному микросервису
+        try {
+            Thread.sleep(20);
+        } catch (InterruptedException e) {
+        }
+
+        Map<Long, Long> goodIdToPriceInRubles = new HashMap<>();
+
+        for (GoodEntity good : goods) {
+            goodIdToPriceInRubles.put(good.getId(), remoteCalculatePriceInRubles(good.getId()));
+        }
+
+        return goodIdToPriceInRubles;
+    }
+
+    private Long remoteCalculatePriceInRubles(Long goodId) {
         return GOOD_ID_TO_PRICE_IN_RUBLES.computeIfAbsent(
-                good.getId(),
-                goodId -> new Random().nextLong(10, 40)
+                goodId,
+                it -> new Random().nextLong(10, 40)
         );
     }
 }
--- shop-api/src/test/java/org/example/shop/api/controllers/GoodsTest.java
+++ shop-api/src/test/java/org/example/shop/api/controllers/GoodsTest.java
@@ -8,15 +8,12 @@ import org.example.shop.store.repositories.GoodRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @Log4j2
