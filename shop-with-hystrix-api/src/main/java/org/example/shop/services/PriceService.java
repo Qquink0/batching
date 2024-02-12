c936883a5bbb8d6771c2beac924454b7bae19efd
Ruslan <overbah98@gmail.com>
2024-02-12 19:36:33 +0700

    hystrix v0.2

--- shop-with-hystrix-api/src/main/java/org/example/shop/services/PriceService.java
+++ shop-with-hystrix-api/src/main/java/org/example/shop/services/PriceService.java
@@ -2,17 +2,19 @@ package org.example.shop.services;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCollapser;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.log4j.Log4j2;
 import org.example.shop.services.remote.PriceServiceApi;
 import org.example.shop.store.entities.GoodEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.Future;
 
+@Log4j2
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @Service
@@ -23,17 +25,16 @@ public class PriceService {
     @HystrixCollapser(
             collapserKey = "getPriceInRubles",
             scope = com.netflix.hystrix.HystrixCollapser.Scope.GLOBAL,
-            batchMethod = "getGoodIdToPriceInRublesMap"
+            batchMethod = "getPriceInRublesByGoods"
     )
     public Future<Long> getPriceInRubles(GoodEntity good) {
         return null;
     }
 
-    @HystrixCommand(
-            groupKey = "goods:price:get-all",
-            commandKey = "getGoodIdToPriceInRublesMap"
-    )
-    public List<Long> getGoodIdToPriceInRublesMap(List<GoodEntity> goods) {
+    @HystrixCommand(commandKey = "getGoodIdToPriceInRublesMap")
+    public List<Long> getPriceInRublesByGoods(List<GoodEntity> goods) {
+
+        log.info("getPriceInRublesByGoods handled %s goods.".formatted(goods.size()));
 
         return priceServiceApi.getGoodIdToPriceInRublesByGoodIds(
                 goods.stream()
