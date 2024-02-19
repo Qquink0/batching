5d5a0cdb779124f3e476e1e25c0524986c4de25c
Ruslan <overbah98@gmail.com>
2024-02-19 19:20:21 +0700

    updated test

--- shop-with-hystrix-api/src/test/java/org/example/shop/api/controllers/GoodsTest.java
+++ shop-with-hystrix-api/src/test/java/org/example/shop/api/controllers/GoodsTest.java
@@ -1,16 +1,21 @@
 package org.example.shop.api.controllers;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.log4j.Log4j2;
+import org.example.shop.api.dto.GoodDto;
 import org.example.shop.store.entities.GoodEntity;
 import org.example.shop.store.repositories.GoodRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Iterator;
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -27,6 +32,9 @@ class GoodsTest {
     MockMvc mvc;
 
     @Autowired
+    ObjectMapper mapper;
+
+    @Autowired
     GoodRepository goodRepository;
 
     @Test
@@ -36,14 +44,42 @@ class GoodsTest {
 
         long getAllGoodsFromControllerStartTime = System.currentTimeMillis();
 
-        mvc.perform(
-                get(GoodsController.GET_ALL_GOODS)
-        )
-                .andExpect(jsonPath("$.size()").value(goodsCount));
+        byte[] contentAsByteArray = mvc.perform(
+                        get(GoodsController.GET_ALL_GOODS)
+                )
+                .andExpect(jsonPath("$.size()").value(goodsCount))
+                .andReturn()
+                .getResponse()
+                .getContentAsByteArray();
+
 
         log.info("All goods fetched by %s millis (controller)."
                 .formatted(System.currentTimeMillis() - getAllGoodsFromControllerStartTime));
 
+        List<GoodDto> goodDtoList = mapper.readValue(contentAsByteArray, new TypeReference<>() {});
+
+        List<GoodEntity> goodEntities = goodRepository.findAll();
+
+        Iterator<GoodDto> goodDtoIterator = goodDtoList.iterator();
+        Iterator<GoodEntity> goodEntityIterator = goodEntities.iterator();
+
+        do {
+
+            boolean hasNextGoodDto = goodDtoIterator.hasNext();
+            boolean hasNextGoodEntity = goodEntityIterator.hasNext();
+
+            Assertions.assertEquals(hasNextGoodDto, hasNextGoodEntity);
+
+            if (!hasNextGoodEntity) {
+                break;
+            }
+
+            GoodDto nextGoodDto = goodDtoIterator.next();
+            GoodEntity nextGoodEntity = goodEntityIterator.next();
+
+            Assertions.assertEquals(nextGoodDto.getId(), nextGoodEntity.getId());
+
+        } while (true);
     }
 
     @Test
